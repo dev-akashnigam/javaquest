a55f19870e862917d8f00a09e261c8405b377ac6
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 15:49:21 +0530

    adding constants wherever necessary in programs for max and min real and decimal numbers

--- 035_MaxNMinDecimalNumber.java
+++ 035_MaxNMinDecimalNumber.java
@@ -2,8 +2,8 @@ class MaxNMinDecimalNumber
 {
     public static void main(String args[])
     {
-        double maxDecimalNo = Double.MAX_VALUE;
-        double minDecimalNo = -Double.MAX_VALUE-1;
+        final double maxDecimalNo = Double.MAX_VALUE;
+        final double minDecimalNo = -Double.MAX_VALUE-1;
 
         System.out.println("Max decimal number supported by Java = "+maxDecimalNo);
         System.out.println("Min decimal number supported by Java = "+minDecimalNo);
