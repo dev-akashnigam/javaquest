cccb8fe5e5e8eb9849797c91e15733bbe622d8a2
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 15:59:48 +0530

    Positive and Negative Infinity in Java

---
+++ 036_PositiveAndNegativeInfinity.java
@@ -0,0 +1,11 @@
+class PositiveAndNegativeInfinity 
+{
+    public static void main(String args[])
+    {
+        final double positiveInfinity = Double.POSITIVE_INFINITY;
+        final double negativeInfinity = Double.NEGATIVE_INFINITY;
+
+        System.out.println("Positive Infinity in Java = "+positiveInfinity);
+        System.out.println("Negative Infinity in Java = "+negativeInfinity);
+    }    
+}
